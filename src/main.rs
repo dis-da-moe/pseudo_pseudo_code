c1bd356217d0319fbfade3b6b784716783c73504
disdamoe <moe.uraijah@gmail.com>
2022-04-18 21:05:54 +0300

    Initial

---
+++ src/main.rs
@@ -0,0 +1,122 @@
+use chumsky::prelude::*;
+
+#[derive(Debug)]
+enum Expr {
+    Num(f64),
+    Var(String),
+
+    Neg(Box<Expr>),
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+    Call(String, Vec<Expr>),
+    Let {
+        name: String,
+        rhs: Box<Expr>,
+        then: Box<Expr>
+    },
+    Fn {
+        name: String,
+        args: Vec<String>,
+        body: Box<Expr>,
+        then: Box<Expr>
+    }
+}
+
+fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
+    let ident = text::ident().padded();
+
+    let expression = recursive(|expr| {
+        let int = text::int(10)
+            .map(|s: String| Expr::Num(s.parse().unwrap()))
+            .padded();
+
+        let atom = int
+            .or(expr.delimited_by(just('('), just(')')))
+            .or(ident.map(Expr::Var));
+
+        let op = |c| just(c).padded();
+
+        let unary = op('-')
+            .repeated()
+            .then(atom)
+            .foldr(|_, right_side| Expr::Neg(Box::new(right_side)));
+
+        let product = unary.clone()
+            .then(op('*').to(Expr::Mul as fn(_, _) -> _)
+                .or(op('/').to(Expr::Div as fn(_, _) -> _))
+                .then(unary)
+                .repeated())
+            .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+
+        let sum = product.clone()
+            .then(op('+').to(Expr::Add as fn(_, _) -> _)
+                .or(op('-').to(Expr::Sub as fn(_, _) -> _))
+                .then(product)
+                .repeated()
+            ).foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+
+        sum
+    });
+
+    let declare = recursive(|decl| {
+        let p_let = text::keyword("let")
+            .ignore_then(ident)
+            .then_ignore(just('='))
+            .then(expression.clone())
+            .then_ignore(just(';'))
+            .then(decl)
+            .map(|((name, rhs), then)| Expr::Let {
+                name,
+                rhs: Box::new(rhs),
+                then: Box::new(then)
+            });
+
+        p_let.or(expression).padded()
+    });
+
+    declare.then_ignore(end())
+}
+
+fn eval<'a>(expr: &'a Expr, vars: &mut Vec<(&'a String, f64)>) -> Result<f64, String> {
+    match expr {
+        Expr::Num(x) => Ok(*x),
+        Expr::Neg(a) => Ok(-eval(a, vars)?),
+        Expr::Add(a, b) => Ok(eval(a, vars)? + eval(b, vars)?),
+        Expr::Sub(a, b) => Ok(eval(a, vars)? - eval(b, vars)?),
+        Expr::Mul(a, b) => Ok(eval(a, vars)? * eval(b, vars)?),
+        Expr::Div(a, b) => Ok(eval(a, vars)? / eval(b, vars)?),
+        Expr::Var(name) => if let Some((_, val)) = vars.iter().rev().find(|(var, _)| *var == name) {
+            Ok(*val)
+        } else {
+            Err(format!("Cannot find variable `{}` in scope", name))
+        },
+        Expr::Let { name, rhs, then } => {
+            let rhs = eval(rhs, vars)?;
+            vars.push((name, rhs));
+            let output = eval(then, vars);
+            vars.pop();
+            output
+        },
+        _ => todo!(),
+    }
+}
+
+fn main() {
+    let source = include_str!("test.foo");
+
+    let parsed = parser().parse(source);
+    match parsed {
+        Ok(ast) => {
+            let evaluated = eval(&ast, &mut Vec::new());
+            match evaluated {
+                Ok(output) => println!("{}", output),
+                Err(error) => println!("Evaluation error: {}", error)
+            }
+        },
+        Err(error) => error
+            .into_iter()
+            .for_each(|err| println!("Parse error: {}", err))
+    }
+}
